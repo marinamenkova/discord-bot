edcaee035afdefa57164176ba641f51e69dde2cf
Марина  Письменкова <marinapismenkova@Mac.attlocal.net>
2025-01-01 22:59:43 -0500

    initial commit

---
+++ bot.py
@@ -0,0 +1,39 @@
+# bot.py
+import os
+import discord
+from dotenv import load_dotenv
+from discord.ext import commands
+
+
+import ssl
+ssl._create_default_https_context = ssl._create_unverified_context
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+load_dotenv()
+TOKEN = os.getenv("DISCORD_TOKEN")
+
+bot=commands.Bot(command_prefix='!', intents=intents)
+
+@bot.event
+async def on_ready():
+   print(f'{bot.user} has connected to discord!')
+
+@bot.event
+async def on_message(message):
+   if message.author==bot.user:
+      return
+   if 'наблюдатель' in message.content.lower():
+    await message.channel.send(f'привет {message.author.mention}')
+    return
+   if message.content.startswith('!'):
+      await bot.process_commands(message)
+      return
+   #await message.channel.send('hey')
+
+@bot.command()
+async def ping(ctx):
+  await ctx.send('help yourself')
+
+bot.run(TOKEN)
